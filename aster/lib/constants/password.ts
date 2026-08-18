1f544b3c3752c74cdb25eadd5e36a995d6f46000
Mohamed Mohamud <moamiin79@gmail.com>
2026-08-19 00:18:04 +0300

    Update default admin password in password constants

--- aster/lib/constants/password.ts
+++ aster/lib/constants/password.ts
@@ -1,2 +1,2 @@
 /** Default password for admin-provisioned accounts (upper, lower, number, 8+ chars) */
-export const DEFAULT_USER_PASSWORD = "Simad@123";
+export const DEFAULT_USER_PASSWORD = "Aster@123";
